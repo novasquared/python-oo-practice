c76e5df4d80406124fdcaa2a8630acbb0afc8a66
jeanne <novasquared@yahoo.com>
2021-09-07 17:13:41 -0500

    implemented SerialGenerator code

--- serial.py
+++ serial.py
@@ -17,3 +17,35 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    """
+    init function
+    - property to store starting number
+    - counter = 0
+
+    generate
+    - every time an instance is created
+        increment counter and return sum
+
+    """
+    def __init__(self, start):
+        """ 
+            initialize self.start to start
+            count to 0
+        """
+        self.start = start
+        self.counter = 0
+    
+    def generate(self):
+        """
+            add start to counter and return sum
+        """
+        sum = self.start + self.counter
+        self.counter += 1
+        
+        return sum
+
+    def reset(self):
+        """
+            reset counter to 0 to reset serial numbers
+        """
+        self.counter = 0
